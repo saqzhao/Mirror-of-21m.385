4f0c88404080beb94b719bd942bd4227b3e61315
Ash <acasden@mit.edu>
2023-05-01 22:42:25 -0400

    title screen

--- code/Main.py
+++ code/Main.py
@@ -7,9 +7,11 @@ from imslib.screen import ScreenManager
 from Serenade import MainScreen
 from StartScreen import IntroScreen
 from FinalSerenadeScreen import EndScreen
+from TitleScreen import TitleScreen
 
 sm = ScreenManager()
 main = MainScreen(name='main')
+sm.add_screen(TitleScreen(name='title'))
 sm.add_screen(IntroScreen(name='intro', start_callback=main.start, interval_callback=main.select_intervals))
 sm.add_screen(main)
 sm.add_screen(EndScreen(name='end', main_screen=main))
---
+++ code/TitleScreen.py
@@ -0,0 +1,57 @@
+import sys, os
+sys.path.insert(0, os.path.abspath('..'))
+
+from imslib.gfxutil import topleft_label, resize_topleft_label
+from imslib.screen import Screen
+from kivy.core.window import Window
+
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy import metrics
+from imslib.gfxutil import CLabelRect
+
+# metrics allows kivy to create screen-density-independent sizes.
+# Here, 20 dp will always be the same physical size on screen regardless of resolution or OS.
+# Another option is to use metrics.pt or metrics.sp. See https://kivy.org/doc/stable/api-kivy.metrics.html
+font_sz = metrics.dp(20)
+button_width = metrics.dp(120)
+button_height = metrics.dp(100)
+
+class TitleScreen(Screen):
+    def __init__(self, **kwargs):
+        super(TitleScreen, self).__init__(always_update=False, **kwargs)
+
+        self.info = topleft_label()
+        self.info.text = "Title Screen\n"
+        self.add_widget(self.info)
+        self.title = Title()
+        self.add_widget(self.title)
+
+        self.start_button = Button(text='Chose Intervals', font_size=font_sz, size = (button_width, button_height), pos = (Window.width/3-button_width/2, Window.height/2-button_height/2))
+        self.start_button.bind(on_release= lambda x: self.switch_to('intro'))
+        self.add_widget(self.start_button)    
+
+        self.levels_button = Button(text='Chose Level (not working)', font_size=font_sz, size = (button_width, button_height), pos = (Window.width*2/3-button_width/2, Window.height/2-button_height/2))
+        self.levels_button.bind(on_release= lambda x: self.switch_to('main'))
+        self.add_widget(self.levels_button)    
+        
+
+    def on_key_down(self, keycode, modifiers):
+        if keycode[1] == 'right':
+            self.switch_to_main()
+
+    # if you want on_update() called when a screen is NOT active, then pass in an extra argument:
+    # always_update=True to the screen constructor.
+    def on_update(self):
+        pass
+
+    def on_resize(self, win_size):
+        # self.start_button.pos = (Window.width/2, Window.height/2)
+        # resize_topleft_label(self.info)
+        pass
+
+class Title(Widget):
+	def __init__(self):
+		super(Title, self).__init__()
+		self.title = CLabelRect(cpos=(Window.width/2, Window.height*3/4), text=f'SERENADE', font_size=40)
+		self.canvas.add(self.title)
